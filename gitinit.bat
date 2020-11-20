d494092c5562a23414b2c1d4ff2297f4c4f5c3eb
hanyidadada <44640043+hanyidadada@users.noreply.github.com>
2020-11-20 16:53:05 +0800

    HAL(除PWM)完成

--- gitinit.bat
+++ gitinit.bat
@@ -2,6 +2,7 @@
 git init
 ::添加远程路径
 git remote add origin https://github.com/WithTenet/STM32
+::保存用户记录
 git config --global credential.helper store
 
 
--- upload.bat
+++ upload.bat
@@ -2,7 +2,7 @@ chcp 65001
 ::添加到暂存区
 git add .
 ::第一次提交
-git commit -m "HAL完成"
+git commit -m "HAL(除PWM)完成"
 ::同步远程仓库
 git pull origin Ye:master --allow-unrelated-histories
 ::推送本地分支到远程
